f9294196e4bdfe90d57b6cc3f8d7b65a92d2b7bb
Luiza <83741493+luizamatos03@users.noreply.github.com>
2021-10-28 16:46:47 -0300

    Update tarefa modulo 03.sql

--- Tarefas modulos/tarefa modulo 03.sql
+++ Tarefas modulos/tarefa modulo 03.sql
@@ -1,24 +1,36 @@
 /* 1. A partir de uma consulta SQL, descubra quantos endereços cadastrados (tabela address) ficam no distrito West
 Bengali. */
-SELECT * FROM sakila.address WHERE district = "West Bengali";
+SELECT * 
+  FROM sakila.address 
+ WHERE district = "West Bengali";
 
 /* 2. Escreva uma consulta SQL que retorne todos os pagamentos (tabela payment) cujo valor da transação
 foi maior que 5 dólares. */
-SELECT * FROM sakila.payment WHERE amount > 5 ; 
+SELECT * 
+  FROM sakila.payment 
+ WHERE amount > 5 ; 
 
 /* 3. Modifique a consulta criada no item 2, para que dessa vez retorne todos os pagamentos cujo valor da
 transação foi maior ou igual 1 dólar e menor que 5 dólares. */
-SELECT * FROM sakila.payment WHERE amount >= 1 AND amount < 5 ;
+SELECT * 
+  FROM sakila.payment 
+ WHERE amount >= 1 AND amount < 5 ;
 
 /* 4. O sistema que determina a classificação indicativa dos filmes é criado e gerenciado pela Motion Picture
 Association. A partir de uma consulta SQL, liste os filmes da locadora que têm indicação de orientação
 parental (valores PG ou PG-13). Há mais de uma solução possível! */
 
-SELECT * FROM sakila.film WHERE rating = "PG" OR rating = "PG-13"
+SELECT * 
+  FROM sakila.film 
+ WHERE rating = "PG" OR rating = "PG-13"
 
-SELECT * FROM sakila.film WHERE rating IN ("PG" ,"PG-13");
+SELECT * 
+  FROM sakila.film 
+ WHERE rating IN ("PG" ,"PG-13");
 
 /* 5. Alguns filmes foram alugados mas não foram devolvidos! A partir da tabela rental, liste os aluguéis que
 não tiveram sua devolução registrada no sistema. */
 
- SELECT * FROM sakila.rental WHERE return_date IS null; 
+ SELECT * 
+   FROM sakila.rental 
+  WHERE return_date IS null; 
