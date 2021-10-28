a33f418e2e34c03491bf7b9d3029220c0c0bb7fd
Luiza <83741493+luizamatos03@users.noreply.github.com>
2021-10-28 16:45:44 -0300

    Update tarefa modulo 04.sql

--- Tarefas modulos/tarefa modulo 04.sql
+++ Tarefas modulos/tarefa modulo 04.sql
@@ -4,11 +4,15 @@ Resposta: Ascendente da coluna rental_id. */
 
 /* b. Qual consulta SQL você pode usar para ordenar os registros de forma descendente por data de retorno do
 DVD? */
-SELECT * FROM sakila.rental ORDER BY return_date DESC;
+SELECT * 
+  FROM sakila.rental 
+ ORDER BY return_date DESC;
 
 /* c. Qual consulta SQL você pode usar para ordenar os registros de forma combinada: ascendente por id do
 consumidor, e ascendente por data de retorno do DVD? */
-SELECT * FROM sakila.rental ORDER BY customer_id, return_date;
+SELECT * 
+  FROM sakila.rental 
+ ORDER BY customer_id, return_date;
 
 /* d. O que acontece com os valores NULL na ordenação?
 Resposta: São exibidos no começo da ordenação. Temos valores null na coluna return_date. Nesse
@@ -17,12 +21,16 @@ outros. */
 
 /*2. Quais são as combinações únicas de features especiais (coluna special_features) dos filmes? Qual consulta você
 utilizou para chegar a esse resultado?*/
-SELECT DISTINCT special_features FROM film;
+SELECT DISTINCT special_features 
+  FROM film;
 
 /* 3. Você está ajudando a equipe de desenvolvimento a criar uma nova funcionalidade no sistema da locadora,
 permitindo que a equipe de atendentes liste os consumidores de maneira similar a seus celulares, ordenando
 por sobrenome ascendente e nome descendente (exemplo abaixo). Qual consulta SQL você pode usar para esse
 objetivo? */
 
-SELECT last_name, first_name FROM customer ORDER BY last_name ASC, first_name DESC;
+SELECT last_name, 
+       first_name 
+  FROM customer 
+ ORDER BY last_name ASC, first_name DESC;
 
