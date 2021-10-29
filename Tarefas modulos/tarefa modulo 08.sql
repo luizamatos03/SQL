e67800986561f9dacf68d6906eaf5889e0ba2d66
Luiza <83741493+luizamatos03@users.noreply.github.com>
2021-10-29 12:35:01 -0300

    Update tarefa modulo 08.sql

--- Tarefas modulos/tarefa modulo 08.sql
+++ Tarefas modulos/tarefa modulo 08.sql
@@ -1,18 +1,16 @@
-/* Além da sintaxe INNER JOIN, estudamos diversas outras opções de relacionamentos entre tabelas SQL, que a
-partir de agora te permitem explorar as mais diversas bases de dados. Não haverá um dia no seu trabalho com
+/* Além da sintaxe INNER JOIN, estudamos diversas outras opções de relacionamentos entre tabelas SQL, que a partir de agora te permitem explorar as mais diversas bases de dados. Não haverá um dia no seu trabalho com
 SQL em que você não irá fazer um relacionamento!
 Antes de caminharmos para o próximo módulo, estude o conteúdo e responda às perguntas:
-1. A gestão da locadora decidiu que no mês de julho de 2005 haverá um prêmio para o funcionário que mais
-registrou vendas, e pediu sua ajuda para determinar a premiação.
-a. Usando a tabela staff como driving table, escreva uma consulta SQL que traga todas as vendas realizadas por
-cada funcionário no mês de julho de 2005. Não é permitido utilizar o filtro WHERE!*/
+	1. A gestão da locadora decidiu que no mês de julho de 2005 haverá um prêmio para o funcionário que mais registrou vendas, e pediu sua ajuda para determinar a premiação.
+		a. Usando a tabela staff como driving table, escreva uma consulta SQL que traga todas as vendas realizadas por
+		cada funcionário no mês de julho de 2005. Não é permitido utilizar o filtro WHERE!*/
 
  SELECT first_name AS Nome, 
-		last_name AS Sobrenome,
-		amount AS Valor_venda, 
-		payment_date AS Data_venda
-   FROM sakila.staff
-  RIGHT JOIN sakila.payment
+	last_name AS Sobrenome,
+	amount AS Valor_venda, 
+	payment_date AS Data_venda
+   FROM staff
+  RIGHT JOIN payment
      ON staff.staff_id = payment.staff_id
  HAVING payment.payment_date BETWEEN '2005-07-01' AND '2005-07-31'
   ORDER BY payment_date;
@@ -24,18 +22,16 @@ cada funcionário no mês de julho de 2005. Não é permitido utilizar o filtro 
  SELECT first_name AS Nome, 
         last_name AS Sobrenome,
         sum(amount) AS Total_vendas
-   FROM sakila.staff
-  RIGHT JOIN sakila.payment
+   FROM staff
+  RIGHT JOIN payment
      ON staff.staff_id = payment.staff_id
   WHERE payment_date BETWEEN '2005-06-01' AND '2005-06-30'
   GROUP BY staff.staff_id
   ORDER BY Total_vendas DESC;
 
-/* 2. Na tarefa do módulo anterior, você descobriu que alguns aluguéis ainda não foram devolvidos. Precisamos
-buscar esses itens! Escreva uma consulta que retorne a lista de emails das pessoas que estão com aluguéis
-pendentes, e o número de itens a serem retornados.
-Bônus opcional: utilize a função GROUP_CONCAT, que estudamos no módulo 5, para retornar também uma lista
-com o nome dos filmes que devem ser devolvidos. */
+/* 2. Na tarefa do módulo anterior, você descobriu que alguns aluguéis ainda não foram devolvidos. Precisamos buscar esses itens! Escreva uma consulta que retorne a 
+lista de emails das pessoas que estão com aluguéis pendentes, e o número de itens a serem retornados.
+Bônus opcional: utilize a função GROUP_CONCAT, que estudamos no módulo 5, para retornar também uma lista com o nome dos filmes que devem ser devolvidos. */
 
 SELECT c.first_name, 
        c.last_name, 
