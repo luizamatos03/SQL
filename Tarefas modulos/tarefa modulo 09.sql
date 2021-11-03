d9e540aaba767c4cd2f13198c3c204a5b2fd7e09
Luiza <83741493+luizamatos03@users.noreply.github.com>
2021-11-02 21:59:08 -0300

    Update tarefa modulo 09.sql

--- Tarefas modulos/tarefa modulo 09.sql
+++ Tarefas modulos/tarefa modulo 09.sql
@@ -4,25 +4,27 @@ a.	Escreva uma consulta que liste, em ordem decrescente, os filmes mais alugados
 Utilize a tabela rental para isso, relacionando com a tabela inventory.*/
 
 SELECT inventory.film_id,
-	   COUNT(inventory.film_id) AS exemplares,
+       COUNT(inventory.film_id) AS exemplares,
        SUM(more_rental.tot_rental) AS alugueis
   FROM inventory 
  INNER JOIN (SELECT inventory_id, 
-			        COUNT(rental_id) AS tot_rental 
-			   FROM rental 
-			  GROUP BY inventory_id 
-			  ORDER BY tot_rental DESC) AS more_rental 
-USING (inventory_id)
-GROUP BY inventory.film_id
-ORDER BY alugueis DESC;
+		    COUNT(rental_id) AS tot_rental 
+		    FROM rental 
+		    GROUP BY inventory_id 
+		    ORDER BY tot_rental DESC) AS more_rental 
+ USING (inventory_id)
+ GROUP BY inventory.film_id
+ ORDER BY alugueis DESC;
 
-/* b.	Escreva uma consulta que liste, em ordem decrescente, o número de unidades disponíveis de cada filme na locadora. */
+/* b. Escreva uma consulta que liste, em ordem decrescente, o número de unidades disponíveis de cada filme na locadora. */
 
-SELECT f.film_id, f.title, COUNT(f.title) AS Qtd
-FROM inventory
-INNER JOIN film AS f USING (film_id) 
-GROUP BY f.title
-ORDER BY Qtd DESC;
+SELECT f.film_id, 
+       f.title, 
+       COUNT(f.title) AS Qtd
+  FROM inventory
+ INNER JOIN film AS f USING (film_id) 
+ GROUP BY f.title
+ ORDER BY Qtd DESC;
 
 /* c.	Usando subconsultas, relacione as tabelas resultantes dos itens a) e b) e responda: 
 os títulos mais alugados de fato correspondem aos que têm maior número de itens disponíveis? 
@@ -34,23 +36,24 @@ RESPOSTA: A consulta abaixo demonstra que os títulos com mais exemplares tem a 
 
 WITH units AS(
 	SELECT f.film_id,
-		   f.title, 
-		   COUNT(f.title) AS Qtd
-      FROM inventory
+	       f.title, 
+	       COUNT(f.title) AS Qtd
+          FROM inventory
 	 INNER JOIN film AS f USING (film_id) 
 	 GROUP BY f.title
 	 ORDER BY qtd DESC),
 
 more_rental AS (
 	 SELECT inventory_id, 
-			COUNT(rental_id) AS tot_rental 
+		COUNT(rental_id) AS tot_rental 
 	   FROM rental 
 	  GROUP BY inventory_id 
 	  ORDER BY tot_rental DESC)
-     
+	  
+-- Query principal
 SELECT i.film_id,
-	   units.title,
-	   COUNT(i.film_id) AS exemplares,
+       units.title,
+       COUNT(i.film_id) AS exemplares,
        SUM(more_rental.tot_rental) AS alugueis
   FROM inventory AS i 
  INNER JOIN more_rental USING (inventory_id)
@@ -61,36 +64,35 @@ SELECT i.film_id,
 
 /*2.	A view chamada sales_by_film_category traz o resultado total de vendas em aluguéis por categoria dos filmes. 
 Escreva uma consulta que complemente essa tabela com as seguintes métricas para cada categoria:
-
 ●	Valor médio do aluguel dos filmes por categoria;
 ●	Valor médio do custo de reposição dos filmes por categoria;*/
 
 -- custos médios por categoria
 WITH medias_custo_reposição AS(
-				  SELECT f.title, 
-						 f_l.category, 
-						 AVG(f.replacement_cost) AS medias_custos_reposição
-				    FROM sakila.film as f
-				   INNER JOIN film_list AS f_l
-					  ON f.title = f_l.title
-				   GROUP BY f_l.category)
+				SELECT f.title, 
+				       f_l.category, 
+				       AVG(f.replacement_cost) AS medias_custos_reposição
+				  FROM sakila.film as f
+				 INNER JOIN film_list AS f_l
+			            ON f.title = f_l.title
+				 GROUP BY f_l.category)
                    
 -- junção dos custos médios por categoria com os custos de reposição por categoria.
-	SELECT f_l.category, 
-           COUNT(f_l.category) AS Qtd_por_categoria, 
-           AVG(price) AS media_preço,
-           (total_sales/COUNT(f_l.category)) AS media_aluguel,
-           medias_custos_reposição
-	  FROM sakila.film_list AS f_l
-	 INNER JOIN sales_by_film_category AS s_c 
-		ON f_l.category = s_c.category
-	 INNER JOIN medias_custo_reposição
-		ON f_l.category = medias_custo_reposição.category
-	 GROUP BY f_l.category;
+SELECT f_l.category, 
+       COUNT(f_l.category) AS Qtd_por_categoria, 
+       AVG(price) AS media_preço,
+       (total_sales/COUNT(f_l.category)) AS media_aluguel,
+       medias_custos_reposição
+  FROM sakila.film_list AS f_l
+ INNER JOIN sales_by_film_category AS s_c 
+    ON f_l.category = s_c.category
+ INNER JOIN medias_custo_reposição
+    ON f_l.category = medias_custo_reposição.category
+ GROUP BY f_l.category;
   
 /*Total de vendas por categoria na loja 1;*/
 SELECT store_id, 
-	   name,
+       name,
        SUM(amount)
   FROM inventory AS i
  INNER JOIN film_category AS f_c USING (film_id)
@@ -104,9 +106,9 @@ SELECT store_id,
 SELECT name AS Categoria,
        SUM(amount) AS soma_vendas,
        CASE
-			WHEN store_id THEN 'Loja 2'
-            ELSE store_id
-			END 'Loja'
+         WHEN store_id THEN 'Loja 2'
+       	 ELSE store_id
+       END 'Loja'
   FROM inventory AS i
  INNER JOIN film_category AS f_c USING (film_id)
  INNER JOIN category AS c USING (category_id)
@@ -116,15 +118,16 @@ SELECT name AS Categoria,
 
 /* ● Total de títulos disponíveis por categoria (dica: use a tabela inventory) */
 WITH categoria_filme AS(
-				SELECT f_c.film_id, 
-					   c.category_id, 
-                       c.name 
-				  FROM film_category AS f_c
-			     INNER JOIN category AS c 
-                    ON f_c.category_id = c.category_id)
+			SELECT f_c.film_id, 
+			       c.category_id, 
+                               c.name 
+			  FROM film_category AS f_c
+			 INNER JOIN category AS c 
+                    	    ON f_c.category_id = c.category_id)
 
-SELECT count(i.inventory_id) AS Qtd, categoria_filme.name 
+SELECT count(i.inventory_id) AS Qtd, 
+       categoria_filme.name 
   FROM inventory AS i
  INNER JOIN categoria_filme
-	ON i.film_id = categoria_filme.film_id
-GROUP BY categoria_filme.name;
+    ON i.film_id = categoria_filme.film_id
+ GROUP BY categoria_filme.name;
