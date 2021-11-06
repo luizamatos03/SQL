e1cf768241a26c531c54b9375aa1a4e66447b72c
Luiza <83741493+luizamatos03@users.noreply.github.com>
2021-11-06 19:29:56 -0300

    Update tarefa modulo 11.sql

--- Tarefas modulos/tarefa modulo 11.sql
+++ Tarefas modulos/tarefa modulo 11.sql
@@ -2,8 +2,8 @@
 	-- a.	A quantidade de aluguéis por mês por loja.
     
     SELECT MONTH(rental_date) AS mes_dos_alugueis,
-		   YEAR(rental_date) AS ano_dos_alugueis,
-		   COUNT(rental_date) AS qtd_alugueis,
+           YEAR(rental_date) AS ano_dos_alugueis,
+           COUNT(rental_date) AS qtd_alugueis,
            staff_id AS Loja
       FROM rental
      GROUP BY MONTH(rental_date), staff_id
@@ -14,23 +14,23 @@
   
   SELECT CONCAT(MONTH(rental_date), " / ", YEAR(rental_date)) AS data_alugueis,
          COUNT(rental_date) AS qtd_alugueis
-	FROM rental
+    FROM rental
    GROUP BY YEAR(rental_date), MONTH(rental_date);
 		 
          
 -- c.	A quantidade de aluguéis por semana entre maio e junho de 2005. Qual semana teve a melhor performance?
 SELECT COUNT(rental_id),
 	   CASE
-		   WHEN p.semana = 21 THEN 'semana de 24 a 28/05/2005'
+	   WHEN p.semana = 21 THEN 'semana de 24 a 28/05/2005'
            WHEN p.semana = 22 THEN 'semana de 29 a 31/05/2005'
            WHEN p.semana = 24 THEN 'semana de 14 a 18/06/2005'
            WHEN p.semana = 25 THEN 'semana de 19 a 21/06/2005'
 	   END AS 'semana'
  FROM (SELECT rental_id,
-	          rental_date,
-			  EXTRACT(WEEK FROM rental_date) AS semana
+	      rental_date,
+              EXTRACT(WEEK FROM rental_date) AS semana
          FROM rental
-		WHERE rental_date >= '2005-05-01' AND rental_date <= '2005-06-30') AS p
+	WHERE rental_date >= '2005-05-01' AND rental_date <= '2005-06-30') AS p
         GROUP BY p.semana;
 
         
@@ -45,10 +45,10 @@ por loja que estão registrados. Dicas de como construir essa consulta:
     
 -- Descubra, primeiro, quem são os consumidores (pelo seu customer_id) que queremos analisar: realizaram dois ou mais aluguéis no mês de maio de 2005.
 SELECT COUNT(r.rental_id) AS qnt_alugueis,
-	   name
+       name
   FROM rental AS r
  INNER JOIN customer_list
-	ON r.customer_id = ID
+       ON r.customer_id = ID
  WHERE MONTH(r.rental_date) = '05'
  GROUP BY name
  ORDER BY qnt_alugueis DESC;
@@ -56,20 +56,20 @@ SELECT COUNT(r.rental_id) AS qnt_alugueis,
 
 -- Depois, avalie a diferença de tempo entre cada aluguel e o seguinte, por consumidor. Você precisará de uma função de janela para isso.
 SELECT name, 
-	   rental_date, 
-	   data_ultm_pedido, 
+       rental_date, 
+       data_ultm_pedido, 
        diferenca_dias,
-	   COUNT(rental_id) OVER (PARTITION BY name ORDER BY name) AS qtd_alugueis
+       COUNT(rental_id) OVER (PARTITION BY name ORDER BY name) AS qtd_alugueis
   FROM (SELECT r.rental_id,
-		       name,
-		       r.rental_date,
-		   LAG (rental_date, 1, rental_date) OVER (PARTITION BY name ORDER BY name, rental_date) AS data_ultm_pedido,
-	            DATEDIFF(rental_date, LAG (rental_date, 1, rental_date) OVER (PARTITION BY name ORDER BY name, rental_date)) AS diferenca_dias
-		  FROM rental AS r
-	     INNER JOIN customer_list
-		    ON r.customer_id = ID
-	     WHERE MONTH(r.rental_date) = '05'
-	     ORDER BY name, rental_date) AS p
+	       name,
+	       r.rental_date,
+	       LAG (rental_date, 1, rental_date) OVER (PARTITION BY name ORDER BY name, rental_date) AS data_ultm_pedido,
+	       DATEDIFF(rental_date, LAG (rental_date, 1, rental_date) OVER (PARTITION BY name ORDER BY name, rental_date)) AS diferenca_dias
+          FROM rental AS r
+	 INNER JOIN customer_list
+            ON r.customer_id = ID
+	 WHERE MONTH(r.rental_date) = '05'
+	 ORDER BY name, rental_date) AS p
  ORDER BY qtd_alugueis DESC, name, data_ultm_pedido;        
     
     
@@ -77,20 +77,20 @@ SELECT name,
 
 WITH p AS          
 	 (SELECT r.rental_id, -- Query para descobrir a diferença dos dias entre os alugueis.
-			 first_name, 
-			 last_name,
-			 r.rental_date,
+	         first_name, 
+	         last_name,
+	         r.rental_date,
 		 LAG (rental_date, 1, rental_date) OVER (PARTITION BY first_name, last_name ORDER BY first_name, rental_date) AS data_ultm_pedido,
 			 DATEDIFF(rental_date, LAG (rental_date, 1, rental_date) OVER (PARTITION BY first_name, last_name ORDER BY first_name, rental_date)) AS diferenca_dias,
 		     store_id
 		FROM rental AS r
-	   INNER JOIN customer USING (customer_id)
-	   WHERE MONTH(r.rental_date) = '05'
-	   ORDER BY first_name, rental_date)
+	  INNER JOIN customer USING (customer_id)
+	  WHERE MONTH(r.rental_date) = '05'
+	  ORDER BY first_name, rental_date)
 	
 SELECT DISTINCT store_id, -- Query principal para a média de dias de cada locadora.
-	   AVG(p.diferenca_dias) OVER (PARTITION BY store_id) AS media_dias
-FROM p;
+       AVG(p.diferenca_dias) OVER (PARTITION BY store_id) AS media_dias
+  FROM p;
 
 
 /* 3.	Reescreva a consulta da tarefa do Módulo 3, dessa vez utilizando um filtro com expressões regulares: 
@@ -106,7 +106,7 @@ SELECT title,
  
  -- Modo 2
 SELECT title,
-	   rating
+       rating
   FROM film_list
  WHERE rating LIKE "PG%";
 
